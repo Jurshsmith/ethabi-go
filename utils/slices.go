038a6ca443d6a7f96e2ba582283558d724506a1b
Joshua Oladele <jurshsmith@gmail.com>
2023-11-05 09:35:55 +0100

    Access reference in slices to avoid value copy

--- utils/slices.go
+++ utils/slices.go
@@ -1,9 +1,9 @@
 package utils
 
-func MapOverSlice[T, V any](ts []T, fn func(T) V) []V {
+func MapOverSlice[T, V any](ts []T, fn func(*T) V) []V {
 	result := make([]V, len(ts))
 	for i, t := range ts {
-		result[i] = fn(t)
+		result[i] = fn(&t)
 	}
 	return result
 }
