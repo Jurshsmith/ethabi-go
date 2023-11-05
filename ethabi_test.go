6c7f0903b7dbfe2280db91ceabe7cac8f8b5d1b1
Joshua Oladele <jurshsmith@gmail.com>
2023-11-05 16:05:33 +0100

    Improve naive test cases

--- ethabi_test.go
+++ ethabi_test.go
@@ -3,11 +3,29 @@ package ethabi
 import (
 	"testing"
 
+	goEthereumAbi "github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParsingEventAbi(t *testing.T) {
-	humanReadableAbi := "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)"
-	abi, _ := ParseABI(&humanReadableAbi)
-	assert.Equal(t, abi.Events["Transfer"].Sig, "Transfer(address,address,uint256)")
+func TestParsingEventAbis(t *testing.T) {
+	humanReadableAbis := []string{
+		"event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)",
+		"event Transfer(address indexed from, address indexed to, uint256 tokenId)",
+		"event Transfer(address indexed from, address indexed to, uint256     tokenId)",
+		"event Transfer(address   indexed    from, address indexed   to, uint256 tokenId)",
+	}
+
+	for _, abi := range humanReadableAbis {
+		abi, err := ParseABI(&abi)
+		assert.Nil(t, err)
+		assert.Equal(t, abi.Events["Transfer"].Sig, "Transfer(address,address,uint256)")
+	}
+}
+
+func TestParsingNonEventAbis(t *testing.T) {
+	invalidHumanReadableAbi := "contract SampleContract{}"
+	abi, err := ParseABI(&invalidHumanReadableAbi)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, abi, goEthereumAbi.ABI{})
 }
